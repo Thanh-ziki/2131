d888719244632bf50045473a14c4645d2a416816
Thanh-ziki <thanh.luongvan2k@gmail.com>
2022-10-07 16:59:29 +0700

    LVT1311

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LuongVanThanh1311.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
